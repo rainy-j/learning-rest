599fe1325d62b626310a3b5d37ad3ab91ca06b8c
rainy-j <90918702+rainy-j@users.noreply.github.com>
2021-11-15 15:53:19 +0000

    set up files

---
+++ src/user/user.controllers.js
@@ -0,0 +1,13 @@
+const User = require("./user.model");
+
+
+exports.addUser = async (req, res) => {
+    try {
+        const newUser = new User(req.body)
+        await newUser.save();
+        res.status(200).send({message: "success" });
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({message: "Something went wrong, check server logs"})
+    }
+}
