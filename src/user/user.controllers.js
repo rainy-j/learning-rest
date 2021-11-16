9a43182be1453d123f281ef2638f3c0b02650a63
rainy-j <90918702+rainy-j@users.noreply.github.com>
2021-11-16 10:13:30 +0000

    second commit

--- src/user/user.controllers.js
+++ src/user/user.controllers.js
@@ -5,7 +5,7 @@ exports.addUser = async (req, res) => {
     try {
         const newUser = new User(req.body)
         await newUser.save();
-        res.status(200).send({message: "success" });
+        res.status(200).send({message: "Success" });
     } catch (error) {
         console.log(error)
         res.status(500).send({message: "Something went wrong, check server logs"})
